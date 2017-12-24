caf6f20a7e49fbde1dab0e6af4e901bd67ba6bfa
ChaseChoi <caiguihao@gmail.com>
2017-12-24 21:12:49 +0800

    sales scanner capture barcode and send back to SalesViewController

---
+++ iBooks/Sales/Controller/ScanViewController.swift
@@ -0,0 +1,78 @@
+//
+//  ScanViewController.swift
+//  iBooks
+//
+//  Created by Chase Choi on 24/12/2017.
+//  Copyright © 2017 Chase Choi. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+
+protocol ScanViewControllerDelegate: class {
+    func scanViewController(_ controller: ScanViewController, finishScanning isbn: String)
+}
+
+class ScanViewController: UIViewController, AVCaptureMetadataOutputObjectsDelegate {
+    weak var delegate: ScanViewControllerDelegate?
+    @IBOutlet weak var scanGridView: UIImageView!
+    var videoPreviewLayer = AVCaptureVideoPreviewLayer()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // setup session
+        let captureSession = AVCaptureSession()
+        guard let captureDevice = AVCaptureDevice.default(for: .video) else {
+            warning(title: "相机识别失败", message: "请使用带有相机的设备", titleForAction: "好")
+            return
+        }
+        
+        do {
+            let input = try AVCaptureDeviceInput(device: captureDevice)
+            captureSession.addInput(input)
+        } catch {
+            print(error)
+        }
+        let output = AVCaptureMetadataOutput()
+        captureSession.addOutput(output)
+        
+        // set delegate
+        output.setMetadataObjectsDelegate(self, queue: .main)
+        output.metadataObjectTypes = [.ean13]
+        
+        videoPreviewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
+        videoPreviewLayer.frame = view.layer.bounds
+        view.layer.addSublayer(videoPreviewLayer)
+        view.bringSubview(toFront: scanGridView)
+        captureSession.startRunning()
+    }
+    func metadataOutput(_ output: AVCaptureMetadataOutput, didOutput metadataObjects: [AVMetadataObject], from connection: AVCaptureConnection) {
+        if metadataObjects.count != 0 {
+            if let object = metadataObjects[0] as? AVMetadataMachineReadableCodeObject {
+                if object.type == .ean13 {
+                    finishScanning(isbn: object.stringValue!)
+                }
+            }
+        }
+        warning(title: "扫描失败", message: "请对准书本条形码", titleForAction: "好")
+        
+    }
+    func warning(title: String, message: String, titleForAction: String) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let action = UIAlertAction(title: titleForAction, style: .default, handler: nil)
+        alert.addAction(action)
+        present(alert, animated: true, completion: nil)
+    }
+    
+    func finishScanning(isbn: String) {
+        let alert = UIAlertController(title: "扫描完成", message: "ISBN: \(isbn)", preferredStyle: .alert)
+        // use closure 
+        let action = UIAlertAction(title: "完成", style: .default, handler: {actions in self.delegate?.scanViewController(self, finishScanning: isbn)} )
+        alert.addAction(action)
+        present(alert, animated: true, completion: nil)
+    }
+    
+}
+
+
